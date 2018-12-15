bff95200230edbf23855b41fbfc5135f25260073
wenbanghack <wenbang.hack@gmail.com>
2018-12-15 16:34:12 +0800

    add style

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var http = require("http");
+let root = document.querySelector("#root");
+let itemTitle = document.querySelectorAll(".item");
 
-http.createServer((req, res)=>{
-
-});
+itemTitle.forEach((item)=>{
+    item.addEventListener("click", function(e){
+        console.log("123");
+        if (this.classList.contains("item__show")) {
+            this.classList.remove("item__show");
+        } else {
+            this.classList.add("item__show");
+        }
+    })
+})
